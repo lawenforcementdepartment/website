5bd2ed0fbeae5988e48ed2ba1138f1ff70848a0c
lawenforcementdepartment <lawenforcementdepartment@proton.me>
2026-04-14 12:13:11 +0100

    Add email bean

---
+++ src/main/java/com/er/led/config/EmailConfig.java
@@ -0,0 +1,32 @@
+package com.er.led.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("SMTP_USERNAME")
+    private String smtpUsername;
+    @Value("SMTP_PASSWORD")
+    private String smtpPassword;
+    @Value("SMTP_HOST")
+    private String smtpHost;
+    @Value("SMTP_PORT")
+    private Integer smtpPort;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(smtpHost);
+        mailSender.setPort(smtpPort);
+
+        mailSender.setUsername(smtpUsername);
+        mailSender.setPassword(smtpPassword);
+
+        return mailSender;
+    }
+}
